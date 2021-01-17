d4bba9e2b1bc65e3cc7c0e715fbec9e58eb0f8e9
Urusa <usaiyed.9127@gmail.com>
2021-01-16 17:55:13 -0700

    object-exercise2

--- Objects/exercise1.js
+++ Objects/exercise1.js
@@ -7,45 +7,32 @@ var student = {
 	rollno: 12
 }
 
-// function getProperties(obj){
-// 	for(let prop in obj){
-// 		console.log(prop);
-// 	}
-// }
-
-// getProperties(student);
-
-//w3resource method 
-
-// function hasKeys(obj){
-// 	if(!isObject) return [];
-// 	if(isObject) return Object.keys(obj);
-// 	var key = [];
-// 	for(let key in obj){
-// 		if((_.has(obj,key))){
-// 			keys.push(key);
-// 			return keys;
-// 		}
-// 	}
-// }
-
-// function isObject(obj){
-
-//   	let type = typeof obj;
-//   	return type === 'function' || type === 'object' && !!obj;
-// }
-
-// console.log(hasKeys(student));
-
-function deleteProp(obj){
-	console.log(obj);
+function getProperties(obj){
 	for(let prop in obj){
-		if(prop === 'rollno'){
-			//console.log(prop);
-			delete obj[prop];
-		}
+		console.log(prop);
 	}
-	return obj;
 }
 
-console.log(deleteProp(student));
+getProperties(student);
+
+w3resource method 
+
+function hasKeys(obj){
+	if(!isObject) return [];
+	if(isObject) return Object.keys(obj);
+	var key = [];
+	for(let key in obj){
+		if((_.has(obj,key))){
+			keys.push(key);
+			return keys;
+		}
+	}
+}
+
+function isObject(obj){
+
+  	let type = typeof obj;
+  	return type === 'function' || type === 'object' && !!obj;
+}
+
+console.log(hasKeys(student));
