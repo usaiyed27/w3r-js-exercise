3e19699f5ae7e3ca1ffae41ffaadc66cbb7217b5
Urusa <usaiyed.9127@gmail.com>
2021-01-22 17:24:28 -0700

    function exercise #5

--- Functions/functions.js
+++ Functions/functions.js
@@ -29,14 +29,33 @@ function findPalindrome(str){
 
 console.log(findPalindrome('Amore, Roma'));
 
+// 5.  Write a JavaScript function that accepts a string as a parameter 
+//and converts the first letter of each word of the string in upper case. Go to the editor
+//Example string : 'the quick brown fox'
+//Expected Output : 'The Quick Brown Fox '
+
 function strCapitalize(str){
     let arr = [];
-    arr = str.split(' ');
-	for(let i = 0; i<arr.length; i++){
-		for(let j = 0; j<arr[i].length; j++){
- 			arr[i] = arr[i].slice(0,1).toUpperCase().concat(arr[i][j+1]);
-			str = arr.join('');
-		}
+    str = str.split(' ');
+
+	for(let i = 0; i<str.length; i++){
+			arr.push(str[i].slice(0,1).toUpperCase() + str[i].slice(1));		
 	}
-return str;
+	str = arr.join(' ');
+	return str;
 }
+
+strCapitalize('the quick brown fox');
+
+// w3resource method
+// function uppercase(str)
+// {
+//   var array1 = str.split(' ');
+//   var newarray1 = [];
+    
+//   for(var x = 0; x < array1.length; x++){
+//       newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+//   }
+//   return newarray1.join(' ');
+// }
+// console.log(uppercase("the quick brown fox"));
