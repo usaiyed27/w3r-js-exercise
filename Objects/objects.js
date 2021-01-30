44fb6f446910268cfdf1251eac5e122c5300545c
Urusa <usaiyed.9127@gmail.com>
2021-01-29 19:22:27 -0700

    modified object folder

--- Objects/objects.js
+++ Objects/objects.js
@@ -36,3 +36,19 @@ function isObject(obj){
 }
 
 console.log(hasKeys(student));
+
+//2.  Write a JavaScript program to delete the rollno property from the following object.
+// Also print the object before or after deleting the property.
+function deleteProp(obj){
+	console.log(obj);
+	for(let prop in obj){
+		if(prop === 'rollno'){
+			//console.log(prop);
+			delete obj[prop];
+		}
+	}
+	return obj;
+}
+
+console.log(deleteProp(student));
+
