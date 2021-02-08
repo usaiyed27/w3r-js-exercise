5438433bc498ff1374492e0282f377a53ede4a5d
Urusa <usaiyed.9127@gmail.com>
2021-02-07 19:26:57 -0700

    'basics'

--- Basic/basic.js
+++ Basic/basic.js
@@ -47,6 +47,7 @@ function calculateDays(){
 	var cmas = new Date(today.getFullYear(), 11, 25);
 	if(today.getMonth()==11 && today.getDate > 25){
 		cmas.setFullYear(cmas.getFullYear() + 1);
+
 	}
 	var one_day = 1000*60*60*24;
 	console.log(Math.ceil((cmas.getTime() - today.getTime())/(one_day)));
